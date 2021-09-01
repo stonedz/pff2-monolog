e8485f030656d00325a379376113bd8311240aa1
Paolo Fagni <paolo.fagni@gmail.com>
2021-09-01 12:21:51 +0200

    php-cs-fixer pass and update for php8.0

--- src/Pff2Monolog.php
+++ src/Pff2Monolog.php
@@ -12,26 +12,31 @@ use pff\Core\ServiceContainer;
 use pff\Iface\IConfigurableModule;
 use Psr\Log\LogLevel;
 
-class Pff2Monolog extends AModule implements IConfigurableModule{
-
+class Pff2Monolog extends AModule implements IConfigurableModule
+{
     private $monolog;
 
-    private $defaultChannelName, $enableFile, $loggerFileName, $enableFirePHP, $addFileInfo;
+    private $defaultChannelName;
+    private $enableFile;
+    private $loggerFileName;
+    private $enableFirePHP;
+    private $addFileInfo;
 
-    public function __construct($confFile = 'pff2-monolog/module.conf.local.yaml') {
+    public function __construct($confFile = 'pff2-monolog/module.conf.local.yaml')
+    {
         $this->loadConfig($confFile);
 
         $this->monolog = new Logger($this->defaultChannelName);
 
-        if($this->enableFile) {
+        if ($this->enableFile) {
             $this->monolog->pushHandler(new StreamHandler(ROOT.'/app/logs/'.date("Y-m-d").'-'.$this->loggerFileName.'.txt'), Logger::INFO);
         }
 
-        if($this->enableFirePHP && ServiceContainer::get('config')->getConfigData('development_environment')) {
+        if ($this->enableFirePHP && ServiceContainer::get('config')->getConfigData('development_environment')) {
             $this->monolog->pushHandler(new FirePHPHandler());
         }
 
-        if($this->addFileInfo) {
+        if ($this->addFileInfo) {
             $this->monolog->pushProcessor(new IntrospectionProcessor());
         }
     }
@@ -40,29 +45,29 @@ class Pff2Monolog extends AModule implements IConfigurableModule{
      * @param array $parsedConfig
      * @return mixed
      */
-    public function loadConfig($parsedConfig) {
-        $conf = $this->readConfig($parsedConfig);
-            $this->defaultChannelName = $conf['moduleConf']['defaultChannelName'];
-
-            $this->enableFile     = $conf['moduleConf']['fileLoggerEnabled'];
-            $this->loggerFileName = $conf['moduleConf']['fileLoggerName'];
-
-            $this->enableFirePHP = $conf['moduleConf']['firePhPLoggerEnabled'];
-
-            $this->addFileInfo = $conf['moduleConf']['addFileInfo'];
+    public function loadConfig($parsedConfig)
+    {
+        $conf                     = $this->readConfig($parsedConfig);
+        $this->defaultChannelName = $conf['moduleConf']['defaultChannelName'];
+        $this->enableFile         = $conf['moduleConf']['fileLoggerEnabled'];
+        $this->loggerFileName     = $conf['moduleConf']['fileLoggerName'];
+        $this->enableFirePHP      = $conf['moduleConf']['firePhPLoggerEnabled'];
+        $this->addFileInfo        = $conf['moduleConf']['addFileInfo'];
     }
 
     /**
      * @return Logger
      */
-    public function getMonolog() {
+    public function getMonolog()
+    {
         return $this->monolog;
     }
 
     /**
      * @param Logger $monolog
      */
-    public function setMonolog($monolog) {
+    public function setMonolog($monolog)
+    {
         $this->monolog = $monolog;
     }
 
@@ -71,53 +76,59 @@ class Pff2Monolog extends AModule implements IConfigurableModule{
      * @param int $level Level to log
      * @param array $context
      */
-    public function log($message, $level, array $context = array()) {
-        if($level<100) {
-            if($level == 0) {
+    public function log($message, $level, array $context = [])
+    {
+        if ($level<100) {
+            if ($level == 0) {
                 $level = Logger::INFO;
-            }
-            elseif($level == 1) {
+            } elseif ($level == 1) {
                 $level = Logger::ERROR;
-            }
-            elseif($level == 2){
+            } elseif ($level == 2) {
                 $level == Logger::CRITICAL;
-            }
-            else {
+            } else {
                 $level = Logger::INFO;
             }
         }
         $this->monolog->log($level, $message, $context);
     }
 
-    public function debug($message, array $context = array()) {
-        $this->monolog->debug($message,$context);
+    public function debug($message, array $context = [])
+    {
+        $this->monolog->debug($message, $context);
     }
 
-    public function info($message, array $context = array()) {
+    public function info($message, array $context = [])
+    {
         $this->monolog->info($message, $context);
     }
 
-    public function notice($message, array $context = array()) {
+    public function notice($message, array $context = [])
+    {
         $this->monolg->notice($message, $context);
     }
 
-    public function warning($message, array $context = array()) {
+    public function warning($message, array $context = [])
+    {
         $this->monolog->warning($message, $context);
     }
 
-    public function error($message, array $context = array()) {
+    public function error($message, array $context = [])
+    {
         $this->monolog->error($message, $context);
     }
 
-    public function critical($message, array $context = array()) {
+    public function critical($message, array $context = [])
+    {
         $this->monolog->critical($message, $context);
     }
 
-    public function alert($message, array $context = array()) {
+    public function alert($message, array $context = [])
+    {
         $this->monolog->alert($message, $context);
     }
 
-    public function emergency($message, array $context = array()) {
+    public function emergency($message, array $context = [])
+    {
         $this->monolog->emergency($message, $context);
     }
 }
